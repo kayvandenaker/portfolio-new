055161847183d2067de7183398a3271d0d4be470
Kay van den Aker <34678030+kayvandenaker@users.noreply.github.com>
2023-09-09 17:41:18 +0200

    test build

--- src/app/protopie/page.js
+++ src/app/protopie/page.js
@@ -11,7 +11,6 @@ export default function Page() {
         <h1>ProtoPie — Highlights of freelance work.</h1>
         
         <p>This is a collection of prototypes and demos I built for ProtoPie, a company creating a no-code prototyping tool. </p>
-
         <h3>Demo: APIs and Parsing JSON</h3>
         <p>Showcasing the new parse json function by fetching data from Spotify and Weather API and feeding it into a car UI. <u><a href='https://www.protopie.io/blog/how-to-integrate-data-into-prototypes' target='blank'>Read tutorial</a></u></p>
         <video src="./media/protopie_json.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
