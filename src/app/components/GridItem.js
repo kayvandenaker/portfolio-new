ab6b06e043c532bff0ca2d55b9a2859d6c5304e1
Kay van den Aker <34678030+kayvandenaker@users.noreply.github.com>
2023-09-10 19:48:01 +0200

    update

--- src/app/components/GridItem.js
+++ src/app/components/GridItem.js
@@ -2,10 +2,19 @@ import {React, useState} from 'react'
 
 export default function GridItem({src, title, info, link}){
 
+    const [windowWidth, setWidth] = useState(window.innerWidth);
+
     return (
         <a className={"gridItem"} href={"/" + link}>
             <div className="gridItemMedia">
-                {src.endsWith("mp4") ? <video onMouseOver={event => event.target.play()} onMouseOut={event => event.target.pause()} width="100%" loop playsInline muted src={src} type="video/mp4"/> : <img src={src}/>}
+                {!src.endsWith("mp4") ? 
+                    <img src={src}/> :
+                    windowWidth > 760 ?
+                        <video onMouseOver={event => event.target.play()} onMouseOut={event => event.target.pause()} width="100%" alt="video" loop playsInline muted src={src} type="video/mp4"/> :
+                        <video width="100%" alt="video" autoPlay loop playsInline muted src={src} type="video/mp4"/>
+                }
+                {/* {src.endsWith("mp4") ? <video width="100%" alt="video" muted src={src} type="video/mp4" poster="./media/midi.jpg"/> : <img src={src}/>} */}
+
             </div>
             <div className="gridItemText">
                 <span>{title}</span>
--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -13,7 +13,6 @@ import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
 export default function Home() {
   const [mouseX, setMouseX] = useState(0);
   const [mouseY, setMouseY] = useState(0);
-  const [contact, setContact] = useState(0);
 
   const headRef = useRef(null);
   const masonryRef = useRef(null);
@@ -28,54 +27,24 @@ export default function Home() {
   return (
     <main>
     <div className='container'>
-
       <div className='intro'>
-        <div>
-          <h1>Kay van den Aker</h1>
           <p>
-          Hey! I'm Kay, a design technologist exploring interactions that fuse digital and physical. I thrive in ambiguous environments using a hands-on and iterative approach focused on experience prototyping; putting concepts in the world by gluing together various hard- and software.
-          <br/><br/>
-          Currently freelancing for <a href='https://www.hapticlabs.io' target="_blank">Hapticlabs</a> and <a href='https://www.protopie.io' target="_blank">ProtoPie</a> while mastering IxD in <a href='https://www.umu.se/en/umea-institute-of-design/' target="_blank">Umeå</a>, formerly at <a href='https://www.above.se' target="_blank">Above</a>, <a href='https://www.arduino.cc' target="_blank">Arduino</a>, <a href='https://www.rolls-roycemotorcars.com/' target="_blank">Rolls-Royce</a> and <a href='https://www.bmw.com' target="_blank">BMW</a>.
-          <br/><br/>
-          <div className='contact-container'>
-            <span className='contact-title' onClick={() => setContact(contact ? false : true)}>{contact? "↘":"→"} <span>Contact</span><br/></span> 
-            <span className={'contact ' + (contact ? "active" : "")}>
-            {/* <span className={'contact active'}> */}
-              <a className='link' href="mailto:kayvandenaker@gmail.com" target="_blank" onClick={() => setContact(false)}>Email</a>
-              <span className='spacer'>/</span>
-              <a className="link" href="https://www.instagram.com/aker.industries/" target="_blank" onClick={() => setContact(false)}>Instagram</a>
-              <span className='spacer'>/</span>
-              <a className="link" href="https://www.linkedin.com/in/kayvandenaker" target="_blank" onClick={() => setContact(false)}>LinkedIn</a>
-              <span className='spacer'>/</span>
-              <a className="link" href="https://twitter.com/kayvandenaker" target="_blank" onClick={() => setContact(false)}>Twitter</a>
-              <span className='spacer'>/</span>
-              <a className="link" href='./files/Kay_van_den_Aker.pdf' target="_blank" onClick={() => setContact(false)}>Resume</a>
-            </span>
-          </div>
+          <b>Kay van den Aker</b><br/><br/>
+          I'm a design technologist exploring interactions that fuse digital and physical. 
           </p>
-        </div>
       </div>
       {columnsCount == 0 ?  <div className='loading'>Media loading...</div> : ""}
 
       <ResponsiveMasonry columnsCountBreakPoints={{350: 1, 760: 2, 1140: 3, 1360: 4}}>
         <Masonry gutter={"20px"} ref={masonryRef} className={"masonry " + (columnsCount ? "loaded" : "")}>
-        {/* <Masonry gutter={"20px"} ref={masonryRef} className={"masonry "}> */}
-        {/* <div className='grid'>
-            <div> */}
-
-          <GridItem src={'./media/tiles.mp4'} title={'Tiles'} info={'Case Study'} link={'tiles'}/>
+          <GridItem src={'./media/tiles_small.mp4'} title={'Tiles'} info={'Case Study'} link={'tiles'}/>
           <GridItem src={'./media/arch.mp4'} title={'Arch'} info={'Case Study'} link={'arch'}/>
           <GridItem src={'./media/touchbar.mp4'} title={'IR TouchBar'} info={'MadMapper / ProtoPie'} link={'touchbar'}/>
           <GridItem src={'./media/protopie_laundry.mp4'} title={'ProtoPie'} info={'Freelance Work'} link={'protopie'}/>
-            {/* </div>
-            <div> */}
           <GridItem src={'./media/hapticlabs_unity.mp4'} title={'Hapticlabs'} info={'Freelance Work'} link={'hapticlabs'}/>
           <GridItem src={'./media/lines.mp4'} title={'Entering Digital'} info={'Unity / Leap Motion'} link={'lines'}/>
           <GridItem src={'./media/uwb.mp4'} title={'Radar Presence'} info={'UWB Radar / ProtoPie'} link={'uwb'}/>
           <GridItem src={'./media/timer_swift.mp4'} title={'Timer Concept'} info={'SwiftUI'} link={'timer'}/>
-
-            {/* </div>
-            <div> */}
           <GridItem src={'./media/bmw.mp4'} title={'BMW Group'} info={'Internship Highlights'} link={'bmw'}/>
           <a className={"gridItem"} href={'/head'}>
             <div className="gridItemMedia" ref={headRef} onMouseMove={(e) => {
@@ -93,18 +62,28 @@ export default function Home() {
           </a>
           <GridItem src={'./media/colors.mp4'} title={'Noise'} info={'Blender'} link={'noise'}/>
           <GridItem src={'./media/bodystorming.mp4'} title={'Bodystorming'} info={'MadMapper / Figma'} link={'bodystorming'}/>
-
-            {/* </div>
-            <div> */}
-
           <GridItem src={'./media/kinect.mp4'} title={'Kitchen'} info={'TouchDesigner / Kinect'} link={'kitchen'}/>
           <GridItem src={'./media/trackpad.mp4'} title={'Trackpad'} info={'Arduino / ProtoPie'} link={'trackpad'}/>
           <GridItem src={'./media/slider.mp4'} title={'Tactile Slider'} info={'Arduino / Processing'} link={'slider'}/>
           <GridItem src={'./media/midi.jpg'} title={'MIDI Controller'} info={'Arduino'} link={'midi'}/>
         </Masonry>
       </ResponsiveMasonry>
-            {/* </div>
-        </div> */}
+      <div className='outro'>
+      <p>
+          <b>About</b><br/><br/>
+          Hey! I'm Kay, a design technologist that thrives in ambiguous environments using a hands-on and iterative approach focused on experience prototyping; putting concepts in the world by gluing together various hard- and software.
+          <br/><br/>
+          Currently freelancing for <a href='https://www.hapticlabs.io' target="_blank">Hapticlabs</a> and <a href='https://www.protopie.io' target="_blank">ProtoPie</a> while mastering IxD in <a href='https://www.umu.se/en/umea-institute-of-design/' target="_blank">Umeå</a>, formerly at <a href='https://www.above.se' target="_blank">Above</a>, <a href='https://www.arduino.cc' target="_blank">Arduino</a>, <a href='https://www.rolls-roycemotorcars.com/' target="_blank">Rolls-Royce</a> and <a href='https://www.bmw.com' target="_blank">BMW</a>.
+          <br/><br/>
+          <div className='contact-container'>
+              <a className='link' href="mailto:kayvandenaker@gmail.com" target="_blank">Email</a>
+              <a className="link" href="https://www.instagram.com/aker.industries/" target="_blank">Instagram</a>
+              <a className="link" href="https://www.linkedin.com/in/kayvandenaker" target="_blank">LinkedIn</a>
+              <a className="link" href="https://twitter.com/kayvandenaker" target="_blank">Twitter</a>
+              <a className="link" href='./files/Kay_van_den_Aker.pdf' target="_blank">Resume</a>
+          </div>
+        </p>
+        </div>
     </div>
     </main>
   )
