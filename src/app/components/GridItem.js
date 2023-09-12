668e0fb173a8adb45ffbfe0da8cdd358944e4211
Kay van den Aker <34678030+kayvandenaker@users.noreply.github.com>
2023-09-12 10:04:07 +0200

    mobile autoplay fix

--- src/app/components/GridItem.js
+++ src/app/components/GridItem.js
@@ -1,25 +1,30 @@
-import {React, useState, useEffect, useRef} from 'react'
+import { React, useEffect, useRef } from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 export default function GridItem({src, title, info, link, poster}){
+
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1140px)' }) ;
     const vidRef = useRef(null);
-    const [w, setW] = useState(600);
 
     useEffect(() => {
-        setW(window.innerWidth);
-        if(src.endsWith("mp4") && window.innerWidth <= 760){
+        if(isTabletOrMobile && src.endsWith("mp4")){
             vidRef.current.play();
-        };
-      }, [])
+        } else if (src.endsWith("mp4")){
+            vidRef.current.pause();
+        }
+    }, [isTabletOrMobile]);
 
     return (
         <a className={"gridItem"} href={"/" + link}>
             <div className="gridItemMedia">
                 {!src.endsWith("mp4") ? 
                     <img src={src}/> :
-                    (w > 760 ?
-                    <video ref={vidRef} onMouseOver={event => event.target.play()} onMouseOut={event => event.target.pause()} width="100%" alt="video" loop playsInline muted src={src} type="video/mp4" poster={poster}/> 
-                    :
-                    <video ref={vidRef} width="100%" alt="video_small" loop playsInline muted src={src} type="video/mp4" poster={poster}/>)
+                    // <video ref={vidRef} {...(isTabletOrMobile ? { autoPlay: true } : {})}  width="100%" loop playsInline muted src={src} type="video/mp4" poster={poster} />
+                    (!isTabletOrMobile ?
+                        <video ref={vidRef} width="100%" loop playsInline muted src={src} type="video/mp4" poster={poster} onMouseOver={event => event.target.play()} onMouseOut={event => event.target.pause()} />
+                        :
+                        <video ref={vidRef} width="100%" loop playsInline muted src={src} type="video/mp4" poster={poster} autoPlay/>
+                    )
                 }
                 {/* {src.endsWith("mp4") ? <video width="100%" alt="video" muted src={src} type="video/mp4" poster="./media/midi.jpg"/> : <img src={src}/>} */}
 
--- src/app/trackpad/page.js
+++ src/app/trackpad/page.js
@@ -10,7 +10,7 @@ export default function Page() {
         <Close />
         <div className='page-hero'>
           <h1>Trackpad</h1>
-          <p>An I2C trackpad visualization, Azoteq TPS65 trackpad sensor as input for ProtoPie.</p>
+          <p>An I2C trackpad visualization, Azoteq TPS65 trackpad sensor as input for ProtoPie. A platform to try different trackpads and explore material overlays.</p>
         </div>
         <video src="./media/trackpad.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
       </div>
