6ea0f9bc5f8ffa56211dde9fc63516a287faf8d2
Kay van den Aker <34678030+kayvandenaker@users.noreply.github.com>
2023-10-12 12:00:57 +0200

    back button fix

--- src/app/components/Close.js
+++ src/app/components/Close.js
@@ -1,13 +1,13 @@
 "use client"
 
 import {React} from 'react'
-// import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+import Link from 'next/link'
+// import { useRouter } from 'next/navigation'
 
 export default function Close(){
-    const router = useRouter()
+    // const router = useRouter();
     return (
-        // <Link className='close' href='/'>← Home</Link>
-        <a className='close' onClick={() => router.back()}>← Home</a>
+        <Link className='close' href='/'>← Home</Link>
+        // <a className='close' onClick={() => router.back()}>← Home</a>
     )
 }
