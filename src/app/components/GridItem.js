c64885d7ad943987f9b9d1976a8c7709789a0961
Kay van den Aker <34678030+kayvandenaker@users.noreply.github.com>
2023-10-13 11:34:27 +0200

    no autoplay on mobile

--- src/app/components/GridItem.js
+++ src/app/components/GridItem.js
@@ -6,18 +6,18 @@ import HeadSmall from '../components/HeadSmall.js';
 export default function GridItem({src, title, info, link, poster}){
 
     const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1140px)' }) ;
-    const vidRef = useRef(null);
+    // const vidRef = useRef(null);
 
-    useEffect(() => {
-        if(vidRef.current){
+    // useEffect(() => {
+    //     if(vidRef.current){
 
-            if(isTabletOrMobile && src.endsWith("mp4")){
-                vidRef.current.play();
-            } else if (src.endsWith("mp4")){
-                vidRef.current.pause();
-            }
-        }
-    }, [isTabletOrMobile]);
+    //         if(isTabletOrMobile && src.endsWith("mp4")){
+    //             vidRef.current.play();
+    //         } else if (src.endsWith("mp4")){
+    //             vidRef.current.pause();
+    //         }
+    //     }
+    // }, [isTabletOrMobile]);
 
     return (
         <Link className={"gridItem"} href={"/" + link}>
@@ -27,11 +27,13 @@ export default function GridItem({src, title, info, link, poster}){
                 <div className="gridItemMedia">
                     {!src.endsWith("mp4") ? 
                         <img src={src}/> :
-                        // <video ref={vidRef} {...(isTabletOrMobile ? { autoPlay: true } : {})}  width="100%" loop playsInline muted src={src} type="video/mp4" poster={poster} />
                         (!isTabletOrMobile ?
-                            <video ref={vidRef} width="100%" loop playsInline muted src={src} type="video/mp4" poster={poster} onMouseOver={event => event.target.play()} onMouseOut={event => event.target.pause()} />
+                            // <video ref={vidRef} width="100%" loop playsInline muted src={src} type="video/mp4" poster={poster} onMouseOver={event => event.target.play()} onMouseOut={event => event.target.pause()} />
+                            <video width="100%" loop playsInline muted src={src} type="video/mp4" poster={poster} onMouseOver={event => event.target.play()} onMouseOut={event => event.target.pause()} />
                             :
-                            <video ref={vidRef} width="100%" loop playsInline muted src={src} type="video/mp4" poster={poster} autoPlay/>
+                            // <video ref={vidRef} width="100%" loop playsInline muted src={src} type="video/mp4" poster={poster}/>
+                            <img src={poster} width="100%"/>
+                            // <video ref={vidRef} width="100%" loop playsInline muted src={src} type="video/mp4" poster={poster} autoPlay/>
                         )
                     }
                     {/* {src.endsWith("mp4") ? <video width="100%" alt="video" muted src={src} type="video/mp4" poster="./media/midi.jpg"/> : <img src={src}/>} */}
