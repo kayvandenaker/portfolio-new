2dba105ba81d64f9b69e3867426aa1f5e4248181
Kay van den Aker <34678030+kayvandenaker@users.noreply.github.com>
2023-10-13 12:58:43 +0200

    portrait video height fix

--- src/app/touchbar/page.js
+++ src/app/touchbar/page.js
@@ -13,7 +13,7 @@ export default function Page() {
           <h1>IR Touchbar</h1>
           <p>An interaction exploration of an infrared touch sensor in combination with MadMapper and ProtoPie. Using textured fabric to touch and projection map onto.</p>
         </div>
-        <video className='portrait' src="./media/touchbar.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
+        <video src="./media/touchbar.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
         <img src="./media/touchbar2.jpeg"/>
         <Nav />
 
