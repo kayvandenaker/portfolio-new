dbe7c6276216937e470cba67a33bfed09f3292df
Kay van den Aker <34678030+kayvandenaker@users.noreply.github.com>
2024-01-07 14:52:07 +0100

    archetype visualization

--- src/app/thesis/page.js
+++ src/app/thesis/page.js
@@ -13,10 +13,16 @@ export default function Page() {
         <h1>Thesis – Physical AI as an interaction design material.</h1>
         <h3>Work In Progress</h3>
         <p>This page is a living document of the master thesis I just started – digging into what physical AI as a material means for the world of interaction design. It aims to explore the possibilities and opportunities that arise when a space understands, reasons, and interacts with the people and things inside of it. How our experiences with our environment change, and our relationship with technology transforms through physical AI. 
-          <br/><br/>
-          If you're interested or want to chat about it, feel free to shoot an <u><a href="mailto:kayvandenaker@gmail.com" target="_blank">email</a></u>.
+          
+          <img src="./media/archetype_overview.jpg"/>
+          <div className='image-details'>Moving the stage of interacting with AI from a screen, to the world around us.</div>
+
+          
           <h3>Partner</h3>
           For this project I'll be collaborating with Archetype AI, a San-Francisco based startup born out of Google's ATAP department (project Soli). Archetype AI leads the way in developing a physical AI platform called a Large Behavior Model, which makes sense of the physical world around it by looking for behavioural patterns through various sensors such as camera, microphone, radar, IMU etcetera.
+
+          <br/><br/>
+          If you're interested or want to chat about it, feel free to shoot an <u><a href="mailto:kayvandenaker@gmail.com" target="_blank">email</a></u>.
 
         </p>
           <div className='context quarter'>
