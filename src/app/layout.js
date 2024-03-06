dd63e4d30560cfe935bb570fa3fbc159d15bb3d0
Kay van den Aker <34678030+kayvandenaker@users.noreply.github.com>
2024-03-06 09:56:02 +0100

    added analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter, Space_Grotesk } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 const space_grotesk = Space_Grotesk({ subsets: ['latin'] })
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <Analytics />
       </body>
     </html>
   )
