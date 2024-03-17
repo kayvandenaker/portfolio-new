eba7e4d46a9d989548d4eb74b18d4faa046d232a
Kay van den Aker <34678030+kayvandenaker@users.noreply.github.com>
2024-03-17 17:47:18 +0100

    Text update

--- src/app/above/page.js
+++ src/app/above/page.js
@@ -9,26 +9,28 @@ export default function Page() {
     return (
       <div className="page">
         <Close />
-        <div className='page-hero'>
-          <h1>Radar Presence</h1>
-          <p>Exploring the possibilities of ultra-wideband radar to detect human presence. Novelda UWB X4 sensor is talking to ProtoPie through a custom Python bridge. ProtoPie is visualizing the data and controlling a NeoPixel through an Adafruit Feather M0.</p>
-        </div>
+        <h1>Tech Explorations</h1>
+        {/* <h3>Summary</h3> */}
+        <p>A collection of small side projects I worked on during my internship at Above, the bigger work is under NDA unfortunately.</p>
+        
+        <h3>Radar Presence</h3>
+        <p>Exploring the possibilities of ultra-wideband radar to detect human presence. Novelda UWB X4 sensor is talking to ProtoPie through a custom Python bridge. ProtoPie is visualizing the data and controlling a NeoPixel through an Adafruit Feather M0.</p>
         <video src="./media/uwb.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
         <video src="./media/uwb2.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
 
 
-        <h1>IR Touchbar</h1>
+        <h3>IR Touchbar</h3>
         <p>An interaction exploration of an infrared touch sensor in combination with MadMapper and ProtoPie. Using textured fabric to touch and projection map onto.</p>
         <video src="./media/touchbar.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
         <img src="./media/touchbar2.jpeg"/>
 
 
-        <h1>Bodystorming</h1>
+        <h3>Bodystorming</h3>
         <p>Projection mapping Figma to get a quick feel for interactions, dimensions and layout. MadMapper screencapturing Figma and projected onto various projection surfaces.</p>
         <video src="./media/bodystorming.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
         <video src="./media/bodystorming2.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
           
-        <h1>Trackpad</h1>
+        <h3>Trackpad</h3>
         <p>An I2C trackpad visualization, Azoteq TPS65 trackpad sensor as input for ProtoPie. A platform to try different trackpads and explore material overlays.</p>
         <video src="./media/trackpad.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
 
--- src/app/side-projects/page.js
+++ src/app/side-projects/page.js
@@ -18,11 +18,13 @@ export default function Page() {
     return (
       <div className="page">
         <Close />
-        <div className='page-hero'>
-          <h1>Pixel Art Generator</h1>
+        {/* <div className='page-hero'> */}
+        <h1>Tech Explorations</h1>
+        <p>A collection of side projects over the years.</p>
+          <h3>Pixel Art Generator</h3>
           <p>Prompting the ChatGPT API to draw an image in an 8x8 grid, outputting it on an LED matrix through Serial.
             Built with React and Arduino, in collaboration with <a href="https://lukasmoro.com" target="_blank">Lukas Moro</a>.</p>
-        </div>
+        {/* </div> */}
         <video src="./media/gpt_smile.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
         <img src="./media/gpt_screenshot.png"/>
         <br/>
@@ -45,56 +47,69 @@ export default function Page() {
           </div>
         </div>
 
-        <h1>Timer</h1>
+        <h3>Timer</h3>
         <p>Micro interaction to go from 0:59 to 1:00, made with SwiftUI.</p>
         <video src="./media/timer_swift.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
         <img src="./media/timer.png"/>
 
 
-        <h1>3D Head</h1>
-        <p>Mouse tracking head made with React Three Fiber and post processing. 3D created with a Lidar scanner, cleaned with MeshMixer and Blender.</p>
+        <h3>3D Head</h3>
+        <p>Mouse tracking head made with React Three Fiber and post processing. 3D created with a Lidar scanner, cleaned with MeshMixer and Blender. Hover over it!</p>
         <div className='head-container' onMouseMove={(e) => {setMouseX(e.clientX); setMouseY(e.clientY)}}>
           <Head mouseX={mouseX} mouseY={mouseY}/>
         </div>
 
-        <h1>Entering Digital</h1>
+        <h3>Entering Digital</h3>
         <p>Distortion as the physical enters the digital world. Loads of particles make the lines in Unity VFX, LeapMotion for the hand tracking.</p>
         <video src="./media/lines.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
         <img src="./media/lines.jpg"/>
 
-        <h1>Shader Noise</h1>
+        <h3>Shader Noise</h3>
         <p>Using shaders to generate colorful animations. Made using various noise textures and gradients in Blender.</p>
         <video src="./media/colors.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
         <img src="./media/colors.png"/>
           
 
-        <h1>Cupra Ferro</h1>
-        <p>We explored how a ferro fluid could co-exist in a vehicle, and represent intelligence in a more abstract way. It moves throughout the vehicle, changes shape and expresses intionality to express itself to the humans inside of the vehicle.<br/><br/>The user changes the shape of the steering wheel when going from driving to speed mode, the driving information moves accordingly from the dashboard to the HUD. </p>
+        <h3>Cupra Ferro</h3>
+        <p>In a collaboration with Cupra, we explored how a ferro fluid could co-exist in a vehicle, and represent intelligence in a more abstract way. It moves throughout the vehicle, changes shape and expresses intionality to express itself to the humans inside of the vehicle.
+        {/* <br/><br/>
+          <ul>
+            <li>Driving: The user changes the shape of the steering wheel when going from driving to speed mode, the driving information moves accordingly from the dashboard to the HUD.</li>
+            <li>Autonomous: The driver enters the centre console when requesting driving information from the fluid about the autonomous journey, the fluid shows its shift of focus accordingly. </li>
+          </ul> */}
+          </p>
+          {/* <br/><br/>The user changes the shape of the steering wheel when going from driving to speed mode, the driving information moves accordingly from the dashboard to the HUD.
+        <br/><br/>The driver enters the centre console when requesting driving information from the fluid about the autonomous journey, the fluid shows its shift of focus accordingly.  */}
+          
+         
         
-        <video src="./media/cupra_driving_bw.mp4" type="video/mp4" controls autoPlay={true} muted loop playsInline>Your browser does not support the video tag.</video>
-        <div className='image-details'>Autonomous -&#62; Driving -&#62; Speed</div>
+        <div className='half'>
+          <div>
+            <video src="./media/cupra_driving_bw.mp4" type="video/mp4" controls autoPlay={true} muted loop playsInline>Your browser does not support the video tag.</video>
+            <div className='image-details'>Autonomous -&#62; Driving -&#62; Speed</div>
+          </div>
+          <div>
+            <video src="./media/cupra_autonomous_bw.mp4" type="video/mp4" controls autoPlay={true} muted loop playsInline>Your browser does not support the video tag.</video>
+            <div className='image-details'>Autonomous -&#62; Touching -&#62; Status</div>
+          </div>
+        </div>
 
-        <p>The driver enters the centre console when requesting driving information from the fluid about the autonomous journey, the fluid shows its shift of focus accordingly. </p>
-        
-        <video src="./media/cupra_autonomous_bw.mp4" type="video/mp4" controls autoPlay={true} muted loop playsInline>Your browser does not support the video tag.</video>
-        <div className='image-details'>Autonomous -&#62; Touching -&#62; Status</div>
-        
-        <p>I created the ferro fluid animations using Blender, combined them with the interface elements in ProtoPie, and projection mapped that onto the physical model. The UI was done by Marie, the CAD by Nikita.</p>
-
-        <h3>Ferro Fluid Explorations</h3>
-        <p>We experimented with various types of ferro fluids, trying different proportions of the chemicals and carrying liquids to explore the behavior and aesthetics of this material.</p>
+        <p>
+          I created the ferro fluid animations using Blender, combined them with the interface elements in ProtoPie, and projection mapped that onto the physical model. The UI was done by <a href="https://mariespreitzer.com/" target="_blank">Marie Spreitzer</a>, the CAD by Nikita Zatonskiy.
+          <br/><br/>
+          We experimented with various types of ferro fluids, trying different proportions of the chemicals and carrying liquids to explore the behavior and aesthetics of this material.</p>
         <video style={{filter: "grayscale(1)"}}  src="./media/cupra_ferro_1.mp4" type="video/mp4" controls autoPlay={true} muted loop playsInline>Your browser does not support the video tag.</video>
         <div className='image-details'>Ferro fluid made using iron powder and motor oil, on a speaker magnet.</div>
 
 
 
-        <h1>Kinect Kitchen</h1>
+        <h3>Kinect Kitchen</h3>
         <p>Projection mapping and image processing in TouchDesigner, Kinect for body tracking.</p>
         <video src="./media/kinect.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
         <img src="./media/kinect.jpg"/>
 
 
-        <h1>MIDI Controller</h1>
+        <h3>MIDI Controller</h3>
         <p>Arduino project from back in 2015, a MIDI Controller for FL Studio. Illuminated arcade buttons, potentionmeters and 5 segment LED filter level indicators.</p>
         <img src="./media/midi.jpg"/>
         <Nav />
