125bfb245b60276c9de9dc116a56405958b4446e
Kay van den Aker <34678030+kayvandenaker@users.noreply.github.com>
2024-05-09 14:10:27 +0200

    added insights

--- src/app/thesis/page.js
+++ src/app/thesis/page.js
@@ -42,43 +42,51 @@ export default function Page() {
 
           <h3>Zero-Shot Object Detection</h3>
           <p>Hand overlapping with input prompts to look for "a photo of a pen".</p>
+          <blockquote>We should not look at models in silos, rather at glueing the right ones together, leveraging their unique qualities.</blockquote>
           <img src="./media/thesis/pen2.png"/>
           <div className='image-details'>MediaPipe and OwlViT (Python).</div>
 
 
           <h3>Accelerometer Analysis</h3>
           <p>LLM making sense of raw sensor data.</p>
+          <blockquote>The more context an LLM has of data, the more accurately it can reason about it.</blockquote>
           <video src="./media/thesis/imu.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
           <div className='image-details'>Adafruit Feather Sense with OpenAI's LLM (Python).</div>
           
 
           <h3>Haptic Glove</h3>
-          <p>Giving feedback through haptics, incorrect is a quick double pulse, correct is a slower single pulse.</p>
+          <p>Feedback through haptics, incorrect is a quick double pulse, correct is a slow single pulse.</p>
+          <blockquote>Translating one sense into another, vision into haptics.</blockquote>
           <video src="./media/thesis/haptics.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
           <div className='image-details'>MediaPipe, YOLO v8 and Hapticlabs (Python).</div>
 
           <h3>Stethoscope</h3>
           <p>Understand the patient together with AI. Stay in the moment with the other person, while listening to the heart beat and getting insights from the AI.</p>
+          <blockquote>Empowerig humans to stay at the centre, instead of replacing them.</blockquote>
           <video src="./media/thesis/stethoscope.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
           <div className='image-details'>A01 Summarize API, OpenAI's LLM/TTS, OpenCV, Librosa and Arduino (Python).</div>
 
           <h3>Binoculars</h3>
           <p>AI powered depth camera. Switch between color and depth view, and get insights from the AI about the main object in the viewport.</p>
+          <blockquote>AI makes sense of the world in a different way.</blockquote>
           <video src="./media/thesis/binoculars.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
           <div className='image-details'>A01 Summarize API, iPhone's LiDAR and Adafruit Feather (Swift).</div>
 
           <h3>Sketch Assistant</h3>
           <p>What if you could sketch with AI together in the physical world? Using traditional tools with an AI layer projected on top.</p>
+          <blockquote>AI can inspire us, if we give it space to do so.</blockquote>
           <video src="./media/thesis/sketch.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
           <div className='image-details'>Latent Consistency Model, Projector and Adafruit Feather (TouchDesigner).</div>
 
           <h3>Dynamic Actuators</h3>
           <p>An LLM deciding what interaction is suitable for the context and the prompt. It can communicate to the person through the desk lamp, the fan, the waving arm and/or the speaker.</p>
+          <blockquote>AI can reason about suitable interactions, with enough context. The relation becomes dynamic, the AI can move from background to foreground, from agent to instrument.</blockquote>
           <video src="./media/thesis/dynamic_actuators.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
           <div className='image-details'>A01 Summarize API, OpenAI's LLM and Arduino (Python).</div>
 
           <h3>AI as a 6th sense</h3>
           <p>What if the space understands your intention, and it could let you feel the relevant information of an object when you interact with it, like a 6th sense. <br/><br/>In this example, a person wants to know if the parcel might be damaged. The AI will pick the most relevant history of the sensor data, and show it through haptics to give the person an understanding of how the parcel was handled, and whether it might be damaged or not.</p>
+          <blockquote> Without changing an object, a whole new personalised set of interactions to make sense of them in their context comes into existence.</blockquote>
           <video src="./media/thesis/box.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
           <div className='image-details'>Hapticlabs, OpenAI's LLM and Adafruit Feather (Python).</div>
 
@@ -88,9 +96,16 @@ export default function Page() {
             <br/><br/> 
             The doctor can express its need for assistance by looking at the screen, indicating he wants the AI to share its insights through that actuator. Subtle facial expressions can be used to get new suggestions, or enlarge them.
           </p>
+          <blockquote>When AI can sense our physical context, it can reason about our intention in that moment – with those people, interacting with those object – and support accordingly.</blockquote>
           <video src="./media/thesis/medical.mp4" type="video/mp4" autoPlay={true} controls muted loop playsInline>Your browser does not support the video tag.</video>
           <div className='image-details'>MediaPipe Face Landmarker (Node.js).</div>
-          
+
+          <h3>Thoughts</h3>
+          <p>
+            All things have character, even if they don't.
+            <br/>
+            Don't underestimate human complexity, emotions, moods, cultures and ethics. 
+          </p>
           <Nav />
 
 
