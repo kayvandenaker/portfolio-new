7ac2266199104723b5f6459a24348240c168f8b1
Kay van den Aker <34678030+kayvandenaker@users.noreply.github.com>
2024-08-18 18:58:11 +0200

    Update Home.js

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -36,7 +36,7 @@ export default function Home() {
           <br /><br />
           {/* Currently doing master thesis <span className='mobileText'>w/</span><span className='desktopText'>with</span> <a href='https://www.archetypeai.io/' target="_blank">Archetype AI</a> in <a href='https://www.umu.se/en/umea-institute-of-design/' target="_blank">Umeå</a><span className="mobileText">. Before</span>
           <span className="desktopText">, formerly</span> at <a href='https://www.hapticlabs.io' target="_blank">Hapticlabs</a>, <a href='https://www.protopie.io' target="_blank">ProtoPie</a>, <a href='https://www.rolls-roycemotorcars.com/' target="_blank">Rolls-Royce</a>, <a href='https://www.above.se' target="_blank">Above</a>, <a href='https://www.arduino.cc' target="_blank">Arduino</a> and <a href='https://www.bmw.com' target="_blank">BMW</a>. */}
-          Currently wrapping up his thesis with <a href='https://www.archetypeai.io/' target="_blank">Archetype AI</a> in <a href='https://www.umu.se/en/umea-institute-of-design/' target="_blank">Umeå</a>, soon freelancing for the <a href='https://ai.google/' target="_blank">Google Envisioning Studio</a>.
+          Currently freelancing for the <a href='https://ai.google/' target="_blank">Google Envisioning Studio</a>.
           <br /><br />
           <span className="contact-container">
             {/* <span className="link" onMouseLeave={() => {document.getElementById("email-tooltip").innerHTML = "copy to clipboard"}} onClick={() => {navigator.clipboard.writeText("kayvandenaker@gmail.com");document.getElementById("email-tooltip").innerHTML = "copied!"}}>Email</span>
