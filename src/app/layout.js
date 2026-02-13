deaaa9cbea6782a3d5da73a20fae124b1efbb7fd
Kay van den Aker <kayakkie87@gmail.com>
2026-02-13 15:16:22 +0100

    header / font update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,4 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: ['400', '700'],
-  variable: '--font-inter',
-});
 
 export const metadata = {
   title: 'Kay van den Aker',
@@ -22,7 +14,7 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         {children}
       </body>
     </html>
